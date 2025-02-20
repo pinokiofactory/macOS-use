b3714eaf3a5ce1886845f699888c14789346fc75
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-20 12:02:22 -0800

    accesibility fix

--- start.js
+++ start.js
@@ -1,27 +1,24 @@
-module.exports = {
-  daemon: true,
-  run: [{
-    method: "shell.run",
-    params: {
-      path: "app",
-      venv: "env",
-      message: [
-        "python gradio_app/app.py"
-      ],
-      env: {
-        "PYTHONPATH": "."
-      },
-      on: [{
-        // Pattern to match Gradio's local URL output
-        "event": "/http:\\/\\/\\S+/",
-        "done": true
-      }]
-    }
-  }, {
-    method: "local.set",
-    params: {
-      // Store the matched URL to display in the UI
-      url: "{{input.event[0]}}"
-    }
-  }]
+module.exports = async (kernel) => {
+  const port = await kernel.port()
+  return {
+    daemon: true,
+    run: [{
+      method: "shell.run",
+      params: {
+        env: {
+          SERVER_PORT: port,
+          LAUNCH_BROWSER: "True"
+        },
+        message: [
+          "osascript -e 'tell app \"Terminal\" to do script \"{{path.resolve(cwd, 'app/env/bin/python')}} {{path.resolve(cwd, 'app/gradio_app/app.py')}}\"'"
+        ],
+      }
+    }, {
+      method: "local.set",
+      params: {
+        // Store the matched URL to display in the UI
+        url: "{{input.event[0]}}"
+      }
+    }]
+  }
 } 
