20e05fe7b3756d03c93a19c212a743cdd8738ef7
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-20 12:09:08 -0800

    update

--- start.js
+++ start.js
@@ -7,7 +7,7 @@ module.exports = async (kernel) => {
       params: {
         env: {
           SERVER_PORT: port,
-          LAUNCH_BROWSER: "True"
+//          LAUNCH_BROWSER: "True"
         },
         message: [
           "osascript -e 'tell app \"Terminal\" to do script \"{{path.resolve(cwd, 'app/env/bin/python')}} {{path.resolve(cwd, 'app/gradio_app/app.py')}}\"'"
@@ -17,7 +17,7 @@ module.exports = async (kernel) => {
       method: "local.set",
       params: {
         // Store the matched URL to display in the UI
-        url: "{{input.event[0]}}"
+        url: "http://127.0.0.1:" + port
       }
     }]
   }
